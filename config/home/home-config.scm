b5c2e96d718cf8fc5fcf6bd1d95f76ae6dfe8ce3
logoraz <erikalmaraz@fastmail.com>
2024-10-27 10:48:20 -0700

    Clean-up and update home-config.

--- config/home/home-config.scm
+++ config/home/home-config.scm
@@ -3,70 +3,50 @@
   #:use-module (gnu home)
   #:use-module (gnu home services)
   #:use-module (gnu home services pm)
-  #:use-module (gnu home services gnupg)
-  #:use-module (gnu home services mcron)
   #:use-module (gnu home services shells)
   #:use-module (gnu home services sound)
   #:use-module (gnu home services desktop)
-  #:use-module (gnu home services dotfiles)
-  #:use-module (gnu home services syncthing)
-  #:use-module (gnu home services guix)
+  ;; #:use-module (gnu home services syncthing)
   #:use-module (guix gexp)
-  #:use-module (config home utils home-files-alist)
   #:use-module (config home services sway-desktop)
   #:use-module (config home services emacs-guile)
+  #:use-module (config home services xdg-files)
+  #:use-module (config home services environment)
   #:use-module (config home services udiskie))
 
-(define %dotdir "/home/logoraz/dotfiles/files/")
-
-(define (dot-files-dir dirname)
-  (string-append %dotdir dirname))
 
 (define home-config
   (home-environment
 
    (services (list
+              ;; Enable sound/bluetooth connections to be handled properly
+              (service home-dbus-service-type)
+
               ;; Enable pipewire audio
               (service home-pipewire-service-type)
 
-              ;; Enable bluetooth connections
-              (service home-dbus-service-type)
-
               ;; XDG files configuration
-              (service home-xdg-configuration-files-service-type
-                       (home-file-dirs->alists
-                        `(,(dot-files-dir "sway")
-                          ,(dot-files-dir "gtk-3.0")
-                          ,(dot-files-dir "emacs")
-                          ,(dot-files-dir "foot")
-                          ,(dot-files-dir "qutebrowser"))))
+              (service home-xdg-local-files-service-type)
 
               ;; Set environment variables for every session
-              (simple-service 'profile-env-vars-service
-                              home-environment-variables-service-type
-                              '( ;; Sort hidden (dot) files first in ls listings
-                                ("LC_COLLATE" . "C")
-                                ;; Set Emacs as editor
-                                ("EDITOR" . "emacs")
-                                ("VISUAL" . "emacs")
-                                ;; Set quotebrowser as the default
-                                ("BROWSER" . "qutebrowser")
-                                ;; Set wayland-specific environment variables
-                                ("XDG_CURRENT_DESKTOP" . "sway")
-                                ("XDG_SESSION_TYPE" . "wayland")
-                                ("RTC_USE_PIPEWIRE" . "true")
-                                ("SDL_VIDEODRIVER" . "wayland")
-                                ("MOZ_ENABLE_WAYLAND" . "1")
-                                ("CLUTTER_BACKEND" . "wayland")
-                                ("ELM_ENGINE" . "wayland_egl")
-                                ("ECORE_EVAS_ENGINE" . "wayland-egl")
-                                ("QT_QPA_PLATFORM" . "wayland-egl")
+              (service home-env-vars-configuration-service-type)
 
-                                ;; Set XDG environment variables
-                                ("XDG_DOWNLOAD_DIR" . "/home/logoraz/Downloads")
-                                ("XDG_PICTURES_DIR" . "/home/logoraz/Pictures/Screenshots")
-                                ("GUILE_WARN_DEPRECATED" . "detailed")))
+              ;; File synchronization
+              ;; (service home-syncthing-service-type)
 
+              ;; Monitor battery levels
+              (service home-batsignal-service-type)
+
+              ;; Udiskie for auto-mounting
+              (service home-udiskie-service-type)
+
+              ;; Emacs profile configuration
+              (service home-emacs-config-service-type)
+
+              ;; Sway Desktop profile configuration
+              (service home-sway-desktop-service-type)
+
+              ;; Bash configuration
               (service home-bash-service-type
                        (home-bash-configuration
                         (guix-defaults? #f)
@@ -79,21 +59,7 @@
                                            #:recursive? #t)))
                         (bash-profile
                          (list (local-file "dot-bash_profile.sh"
-                                           #:recursive? #t)))))
-              ;; File synchronization
-              (service home-syncthing-service-type)
+                                           #:recursive? #t)))))))))
 
-              ;; Monitor battery levels
-              (service home-batsignal-service-type)
-
-              ;; Udiskie for auto-mounting
-              (service home-udiskie-service-type)
-
-              ;; Emacs configuration
-              (service home-emacs-config-service-type)
-
-              ;; Sway Desktop configuration
-              (service home-sway-desktop-service-type)))))
-
-
+;; Instantiated home configuration
 home-config
